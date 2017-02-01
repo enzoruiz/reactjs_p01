93c1acdc4cbb9efc42d375f7e46649dcf56158b8
enzo ruiz <enzo.rp.90@gmail.com>
2017-02-01 11:05:53 -0500

    props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,8 +5,24 @@ export class Home extends React.Component{
 		return(
 			<div>
 				<p>El componente de Home</p>
-				{ 2 + 2 }
+				<p>Tu nombre es {this.props.name}</p>
+				<p>Tu edad es {this.props.age}</p>
+				<div>
+					<h4>Tus peliculas</h4>
+					<ul>
+						{this.props.movies.map((pelicula, i) => <li key={i}>{pelicula}</li>)}
+					</ul>
+				</div>
+				<hr/>
+				{this.props.children}
 			</div>
 		);
 	}
+}
+
+Home.propTypes = {
+	name: React.PropTypes.string,
+	age: React.PropTypes.number,
+	movies: React.PropTypes.array,
+	children: React.PropTypes.element.isRequired
 }
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,7 @@ import { Home } from "./components/Home.js";
 
 class App extends React.Component{
 	render(){
+		var movies = ["Avengers", "Spiderman"]
 		return (
 			<div className="container">
 				<div className="row">
@@ -14,7 +15,9 @@ class App extends React.Component{
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home/>
+						<Home name={"Pepe"} age={30} movies={movies}>
+							<p>AAAAAAAAAA</p>
+						</Home>
 					</div>
 				</div>
 			</div>
