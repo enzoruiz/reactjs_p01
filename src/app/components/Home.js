d5e8e86fed0ab85168687742b012f1673adb6fe8
enzo ruiz <enzo.rp.90@gmail.com>
2017-02-01 10:34:06 -0500

    creacion de componentes

---
+++ src/app/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Home extends React.Component{
+	render(){
+		return(
+			<div>
+				<p>El componente de Home</p>
+				{ 2 + 2 }
+			</div>
+		);
+	}
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,24 @@
-console.log("no pos funciona!");
+import React from "react";
+import { render } from "react-dom";
+import { Header } from "./components/Header.js";
+import { Home } from "./components/Home.js";
+
+class App extends React.Component{
+	render(){
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Header/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Home/>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+render(<App/>, window.document.getElementById("app"));
