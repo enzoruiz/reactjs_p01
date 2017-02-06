65ad051ac3fe02ef825ce2cf589520e6b0d2bf5b
enzo ruiz <enzo.rp.90@gmail.com>
2017-02-06 12:36:07 -0500

    redux

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,13 +4,24 @@ export class Home extends React.Component{
 	constructor(props){
 		super();
 		this.state = {
-			age: props.initialAge
+			age: props.initialAge,
+			homeLink: "El nuevo nombre del Link"
 		}
 	}
 
 	onAumentarEdad(){
 		this.setState({
 			age: this.state.age + 3
+		});
+	}
+
+	onNuevoNombreLink(){
+		this.props.nuevoNombreLink(this.state.homeLink);
+	}
+
+	onGetTextInput(event){
+		this.setState({
+			homeLink: event.target.value
 		});
 	}
 
@@ -27,9 +38,12 @@ export class Home extends React.Component{
 					</ul>
 				</div>
 				<hr/>
-				{this.props.children}
-				<hr/>
 				<button onClick={() => this.onAumentarEdad()} className="btn btn-primary">Aumentar Edad</button>
+				<hr/>
+				<button onClick={this.props.mensaje} className="btn btn-primary">Mostrar Mensaje</button>
+				<hr/>
+				<input onChange={(event) => this.onGetTextInput(event)} type="text" value={this.state.homeLink}/>
+				<button onClick={() => this.onNuevoNombreLink()} className="btn btn-primary">Cambiar Nombre Link</button>
 			</div>
 		);
 	}
@@ -39,5 +53,6 @@ Home.propTypes = {
 	name: React.PropTypes.string,
 	initialAge: React.PropTypes.number,
 	movies: React.PropTypes.array,
-	children: React.PropTypes.element.isRequired
+	mensaje: React.PropTypes.func,
+	nuevoNombreLink: React.PropTypes.func
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,27 +1,96 @@
-import React from "react";
-import { render } from "react-dom";
-import { Header } from "./components/Header.js";
-import { Home } from "./components/Home.js";
+// import React from "react";
+// import { render } from "react-dom";
+// import { Header } from "./components/Header.js";
+// import { Home } from "./components/Home.js";
 
-class App extends React.Component{
-	render(){
-		var movies = ["Avengers", "Spiderman"]
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-xs-10 col-xs-offset-1">
-						<Header/>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-xs-10 col-xs-offset-1">
-						<Home name={"Pepe"} initialAge={30} movies={movies}>
-							<p>AAAAAAAAAA</p>
-						</Home>
-					</div>
-				</div>
-			</div>
-		);
-	}
+// class App extends React.Component{
+// 	constructor(){
+// 		super();
+// 		this.state = {
+// 			homeLink: "Home"
+// 		}
+// 	}
+
+// 	onCambiarNuevoNombre(nuevoNombre){
+// 		this.setState({
+// 			homeLink: nuevoNombre
+// 		});
+// 	}
+
+// 	onMostrarMensaje(){
+// 		alert("Holaaaaaaaaaaa");
+// 	}
+
+// 	render(){
+// 		var movies = ["Avengers", "Spiderman"]
+// 		return (
+// 			<div className="container">
+// 				<div className="row">
+// 					<div className="col-xs-10 col-xs-offset-1">
+// 						<Header homeLink={this.state.homeLink}/>
+// 					</div>
+// 				</div>
+// 				<div className="row">
+// 					<div className="col-xs-10 col-xs-offset-1">
+// 						<Home 
+// 							name={"Pepe"} 
+// 							initialAge={30} 
+// 							movies={movies} 
+// 							mensaje={this.onMostrarMensaje}
+// 							nuevoNombreLink={(nuevoNombre) => this.onCambiarNuevoNombre(nuevoNombre)} />
+// 					</div>
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+// render(<App/>, window.document.getElementById("app"));
+
+// USANDO REDUX
+import { createStore, combineReducers } from "redux";
+
+const  initialState = {
+	result: 1,
+	lastValues: []
 }
-render(<App/>, window.document.getElementById("app"));
+
+const reducer = (state = initialState, action) => {
+	switch(action.type){
+		case "ADD":
+			state = {
+				...state,
+				result: state.result + action.payload,
+				lastValues: [...state.lastValues, action.payload]
+			}
+			break;
+		case "SUBTRACT":
+			state = {
+				...state,
+				result: state.result - action.payload,
+				lastValues: [...state.lastValues, action.payload] 
+			}
+			break;
+	}
+	return state;
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+	console.log("Store actualizado", store.getState());
+});
+
+store.dispatch({
+	type: "ADD",
+	payload: 15
+});
+
+store.dispatch({
+	type: "ADD",
+	payload: 50
+});
+
+store.dispatch({
+	type: "SUBTRACT",
+	payload: 10
+});
